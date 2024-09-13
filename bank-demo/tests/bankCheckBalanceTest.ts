8c3c6191fb505066b265fe983458fb2222472f38
Caroline Yu <carolineyu@Carolines-MacBook-Pro.local>
2024-09-13 11:37:08 -0400

    resolved #2

--- bank-demo/tests/bankCheckBalanceTest.ts
+++ bank-demo/tests/bankCheckBalanceTest.ts
@@ -3,7 +3,9 @@ import Bank from "../src/bank";
 //senario 1
 const bank = new Bank();
 const acc = bank.createAccount('Jane Doe', 25, '123456');
-if(acc.accountNumber === "123456"){
+const balance = bank.checkBalance('123456');
+
+if(acc.balance === 0){
     console.log("scenario 1 passed");
 }else{
     console.log("scenario 1 failed.");
@@ -12,9 +14,10 @@ if(acc.accountNumber === "123456"){
 
 //scenario 2
 try{
-    bank.createAccount('Jane Doe', 2, '123456');
+    bank.checkBalance('123455');
     console.log("scenario 2 failed");
 }
 catch(e){
     console.log("scenario 2 passed");
+
 }
